4c0d6a0324cfe1e1727ebf2bc6546321fe73d82e
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-01-17 15:45:59 +0200

    add actions in post and comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,22 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
+  class CommentsController < ApplicationController
+    def create
+      @post = Article.find(params[:article_id])
+      @comment = @post.comments.create(comment_params)
+
+      if @comment.save
+        redirect_to article_path(@articles)
+      else
+        render :new
+      end
+    end
+
+    private
+
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
+  end
 end
