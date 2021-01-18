8c6ffed7034dea7d26452a7df90ef9f99e1aa6a8
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-01-18 17:01:06 +0200

    add login view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  # before_action :require_login
+
   private
+
+  # def require_login
+  #   unless logged_in?
+  #     flash[:error] = 'You must be logged in to access this section'
+  #     redirect_to new_session_path
+  #   end
+  # end
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[user_id]
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,25 @@
 class SessionsController < ApplicationController
   def index
+    redirect_to sessions_path
   end
-  
+
   def new
-    @user = User.new
+    # @user = User.new
   end
 
   def create
     user = User.find_by_username(params[:username])
     if user && User.authenticate(params[:password])
       session[:user.id] = user.id
-      redirect_to root_path, notice: 'You successfully signed in!'
+      redirect_to new_session_path, notice: 'You successfully signed in!'
     else
       render :new, alert: 'Invalid username or password'
     end
+  end
+
+  def destroy
+    session.delete(:current_user_id)
+    @current_user = nil
+    redirect_to new_path
   end
 end
