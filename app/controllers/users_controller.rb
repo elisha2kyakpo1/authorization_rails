7f4334f2adc753817dbad38a883455b2aaa90035
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-01-20 08:23:11 +0200

    add a new user into session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      session[:user_id]
+      session[:user_id] = @user.id
       redirect_to posts_path
     else
       render :new
